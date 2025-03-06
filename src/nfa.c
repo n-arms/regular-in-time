819c6bde81c5db42a8bb23e7b81bc6ab4a6c960d
n-arms <75282394+n-arms@users.noreply.github.com>
2025-03-06 02:08:42 -0500

    Implement nfa pattern matching

--- src/nfa.c
+++ src/nfa.c
@@ -1,7 +1,47 @@
 #include "nfa.h"
+#include "nodelist.h"
 
 /// Determine if the nfa matches a null-terminated string.
 bool matches(nfa *nfa, char *str) {
+  node_list nodes = new_node_list();
+  push_node_list(&nodes, nfa->start);
+
+  for (size_t i = 0; str[i] != '\0'; ++i) {
+    for (size_t j = 0; j < nodes->length; ++j) {
+      nfa_state state = nfa->states[j];
+      for (size_t k = 0; k < state.size; ++k) {
+        nfa_edge edge = state.edges[k];
+        if (edge.on_char == '\0') {
+          push_node_list(&nodes, edge.next);
+        }
+      }
+    }
+
+    node_list next_nodes = new_node_list();
+    char c = str[i];
+
+    for (size_t j = 0; j < node->length; ++j) {
+      nfa_state state = nfa->states[j];
+      for (size_t k = 0; k < state.size; ++k) {
+        nfa_edge edge = state.edges[k];
+        if (edge.on_char == c) {
+          push_node_list(&next_nodes, edge.next);
+        }
+      }
+    }
+
+    free_node_list(&nodes);
+    nodes = next_nodes;
+  }
+
+  for (size_t i = 0; i < nodes->length; ++i) {
+    if (nodes[i] == nfa->end) {
+      free_node_list(&nodes);
+      return true;
+    }
+  }
+
+  free_node_list(&nodes);
   return false;
 }
 

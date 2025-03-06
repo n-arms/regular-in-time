d256193931d836ef42c3a89ac8d3b5e74f9d6297
n-arms <75282394+n-arms@users.noreply.github.com>
2025-03-06 02:15:35 -0500

    Fix typos in nfa automata matching

--- src/nfa.c
+++ src/nfa.c
@@ -4,15 +4,15 @@
 /// Determine if the nfa matches a null-terminated string.
 bool matches(nfa *nfa, char *str) {
   node_list nodes = new_node_list();
-  push_node_list(&nodes, nfa->start);
+  push_to_node_list(&nodes, nfa->start);
 
   for (size_t i = 0; str[i] != '\0'; ++i) {
-    for (size_t j = 0; j < nodes->length; ++j) {
+    for (size_t j = 0; j < nodes.length; ++j) {
       nfa_state state = nfa->states[j];
       for (size_t k = 0; k < state.size; ++k) {
         nfa_edge edge = state.edges[k];
         if (edge.on_char == '\0') {
-          push_node_list(&nodes, edge.next);
+          push_to_node_list(&nodes, edge.next);
         }
       }
     }
@@ -20,12 +20,12 @@ bool matches(nfa *nfa, char *str) {
     node_list next_nodes = new_node_list();
     char c = str[i];
 
-    for (size_t j = 0; j < node->length; ++j) {
+    for (size_t j = 0; j < nodes.length; ++j) {
       nfa_state state = nfa->states[j];
       for (size_t k = 0; k < state.size; ++k) {
         nfa_edge edge = state.edges[k];
         if (edge.on_char == c) {
-          push_node_list(&next_nodes, edge.next);
+          push_to_node_list(&next_nodes, edge.next);
         }
       }
     }
@@ -34,8 +34,8 @@ bool matches(nfa *nfa, char *str) {
     nodes = next_nodes;
   }
 
-  for (size_t i = 0; i < nodes->length; ++i) {
-    if (nodes[i] == nfa->end) {
+  for (size_t i = 0; i < nodes.length; ++i) {
+    if (nodes.elems[i] == nfa->end) {
       free_node_list(&nodes);
       return true;
     }
